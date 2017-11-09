76dc22e410405d90bbc34ceb7012852183044b97
tnx6devine <tnx6devine@gmail.com>
2017-11-08 19:33:32 -0500

    first tests

---
+++ personalinfo.rb
@@ -0,0 +1,11 @@
+def age()
+	puts "How old are you?"
+	age = gets.chomp
+	age.to_i
+end
+
+def birthMonth()
+	puts "What month were you born in?"
+	month = gets.chomp
+	month
+end
---
+++ personalinfo_test.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+require_relative "personalinfo.rb"
+class TestPersonalInfo < Minitest::Test
+    
+    def test_assert_that_1_equals_1
+        assert_equal(1, 1)
+    end
+
+    def test_assert_that_18_equals_18
+    	assert_equal(18, age())
+    end
+
+    def test_assert_that_January_is_January
+    	assert_equal("January", birthMonth())
+    end
+
+end
